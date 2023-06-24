49de66e7c4f7a794dcc90984f2cf78630340ac21
Daniel Oladele <50686796+danieloladele7@users.noreply.github.com>
2023-06-24 03:14:56 +0000

    task 1 correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,10 @@
 exec { 'puppet-lint':
   command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
+
+# install flask -v 2.1.0
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
